80cc74ef3b0ee9d9b27efa638aaa29be66b03f90
Nasir <nasir.rashid564@gmail.com>
2025-02-07 11:43:33 +0000

    title

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 const Dashboard = () => {
   return (
     <>
-    <h1></h1>
+    <h1>Dashboard</h1>
     <Link to='/'>Main Menu</Link>
     </>
   )
