952d7ff01966e05d54e3d8114a65ba9312e48397
Nasir <nasir.rashid564@gmail.com>
2025-02-11 11:39:34 +0000

    tailwind css

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: {
+      tailwindcss: {}, // this should be included to process Tailwind
+      autoprefixer: {}, // this will handle vendor prefixing
+    },
+  };
+  
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}', // Path to all your components and pages
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
